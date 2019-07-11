83773cfdaa46792ed8d6713f7775f8cf24173bed
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-07-11 18:41:48 +0000

    add ascii art

--- calculator.py
+++ calculator.py
@@ -1,9 +1,22 @@
 numbers = []
 cont = "yes"
+print("welcome to the calc: ")
+print(" _____________________")
+print("|  _________________  |")
+print("| | JO           0. | |")
+print("| |_________________| |")
+print("|  ___ ___ ___   ___  |")
+print("| | 7 | 8 | 9 | | + | |")
+print("| |___|___|___| |___| |")
+print("| | 4 | 5 | 6 | | - | |")
+print("| |___|___|___| |___| |")
+print("| | 1 | 2 | 3 | | x | |")
+print("| |___|___|___| |___| |")
+print("| | . | 0 | = | | / | |")
+print("| |___|___|___| |___| |")
+print("|_____________________|")
 op = input("What operation would you like to do? ") 
-
 while cont == "yes":
-    
     numbers.append(int(input("what is the next number? ")))
     cont = input("yes or no, would you like to calculate more numbers? ")
 if op == "add":
@@ -18,8 +31,17 @@ if op == "multiply":
     print(mult)
 if op == "subtract":
     sub = numbers[0]
-    for i in numbers:
-        sub -= (i + 1)
+    for i in range(1,len(numbers)):
+        sub -= numbers[i]
     print(sub)
-    
+if op == "divide":
+    div = numbers[0]
+    for i in range(1,len(numbers)):
+        div /= numbers[i]
+    print(div)
+if op == "exponents":
+    exp = numbers[0]
+    for i in range(1,len(numbers)):
+        exp **= numbers[i]
+    print(exp)
 print(numbers)
