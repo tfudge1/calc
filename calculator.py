577a65742713cc037ed7897232562356e35e078a
EC2 Default User <ec2-user@ip-172-31-26-90.us-east-2.compute.internal>
2019-07-11 18:11:00 +0000

    subtraction

--- calculator.py
+++ calculator.py
@@ -1,6 +1,25 @@
 numbers = []
-cont = input("yes or no, would you like to calculate more numbers? ")
-if cont == "yes":
+cont = "yes"
+op = input("What operation would you like to do? ") 
+
+while cont == "yes":
+    
     numbers.append(int(input("what is the next number? ")))
+    cont = input("yes or no, would you like to calculate more numbers? ")
+if op == "add":
+    sums = 0
+    for i in numbers:
+        sums += i
+    print(sums)
+if op == "multiply":
+    mult = 1
+    for i in numbers:
+        mult *= i
+    print(mult)
+if op == "subtract":
+    sub = numbers[0]
+    for i in numbers:
+        sub -= (i + 1)
+    print(sub)
     
 print(numbers)
