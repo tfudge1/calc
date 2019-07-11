4d4db8d65bce99bf9a4258a91f05f0109a8b1ade
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-07-11 17:33:40 +0000

    started with the loop and the inputs

---
+++ calculator.py
@@ -0,0 +1,6 @@
+numbers = []
+cont = input("yes or no, would you like to calculate more numbers? ")
+if cont == "yes":
+    numbers.append(int(input("what is the next number? ")))
+    
+print(numbers)
